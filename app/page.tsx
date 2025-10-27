0a11d94df58df5ecffcdde4f6f0f1660c4da49e7
ata <atak2831@gmail.com>
2025-10-27 17:35:42 +0330

    [UI]: better visualizing the final code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 type Binary = 1 | 0;
 type BinaryList = Binary[];
@@ -14,6 +14,15 @@ type Levels = {
 
 export default function Home() {
   const [showLevel, setShowLevel] = useState<number>(0);
+  const [crcCode, setCrcCode] = useState<BinaryList>();
+  const [drawAnswerBox, setDrawAnswerBox] = useState({
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  });
+  const answerBoxLeft = useRef(null);
+  const answerBoxRight = useRef(null);
   // const data: BinaryList = useMemo(() => [1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1], []);
   const data: BinaryList = useMemo(() => [1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0], []);
   const key: BinaryList = useMemo(() => [1, 0, 1, 0, 1], []);
@@ -69,8 +78,29 @@ export default function Home() {
       };
     }
 
+    setCrcCode(
+      localLevels[data.length - key.length + 1].list.slice(
+        data.length - key.length + 1
+      )
+    );
     return localLevels;
   }, []);
+
+  useEffect(() => {
+    if (!answerBoxLeft.current && !answerBoxRight.current) return;
+
+    //@ts-expect-error never null
+    const left = answerBoxLeft.current.getBoundingClientRect();
+    //@ts-expect-error never null
+    const right = answerBoxRight.current.getBoundingClientRect();
+
+    setDrawAnswerBox({
+      left: left.left,
+      top: left.top,
+      bottom: left.bottom,
+      right: right.right,
+    });
+  }, [answerBoxLeft, answerBoxRight]);
 
   return (
     <div className="w-full min-h-screen p-20 ">
@@ -99,45 +129,69 @@ export default function Home() {
       <div className="w-fit">
         {data.length > 0 &&
           Object.entries(levels).map(([level, values]) => (
-            <div
-              key={level}
-              className={`transition duration-300 relative ${
-                +level <= showLevel ? "opacity-100" : "opacity-0"
-              } `}
-            >
-              <div className="**:ml-8 ">
-                <div>
-                  <span className="text-gray-600 absolute left-0">{level}</span>
-                  {values.list.map((value, i) => (
-                    <span
-                      className={
-                        i < values.shiftIndex + 1
-                          ? "relative text-red-300 before:absolute before:w-4 before:h-0.5 before:bg-red-300 before:-rotate-45 before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2"
-                          : values.answer
-                          ? "underline text-indigo-500"
-                          : i === values.list.length - 1 && i >= key.length
-                          ? "text-emerald-500"
-                          : "text-white"
-                      }
-                      key={i}
-                    >
-                      {value}
+            <div key={level}>
+              {+level <= showLevel && values.answer && (
+                <div
+                  style={{
+                    width: `${drawAnswerBox.right - drawAnswerBox.left + 10}px`,
+                    height: `${
+                      drawAnswerBox.bottom - drawAnswerBox.top + 10
+                    }px`,
+                    left: `${drawAnswerBox.left - 5}px`,
+                    top: `${drawAnswerBox.top - 5}px`,
+                  }}
+                  className="absolute border border-indigo-500 rounded-md bg-indigo-950"
+                />
+              )}
+              <div
+                className={`transition duration-300 relative ${
+                  +level <= showLevel ? "opacity-100" : "opacity-0"
+                } `}
+              >
+                <div className="**:ml-8 ">
+                  <div>
+                    <span className="text-gray-600 absolute left-0">
+                      {level}
                     </span>
-                  ))}
-                </div>
-                {data.length > 0 && !values.answer && (
-                  <div className="relative text-gray-600">
-                    <span className="absolute -left-12 text-sm">Key:</span>
-                    {Array.from({ length: +level }).map((_, i) => (
-                      <span className="opacity-0" key={i}>
-                        0
+                    {values.list.map((value, i) => (
+                      <span
+                        ref={
+                          i === data.length - key.length + 1
+                            ? answerBoxLeft
+                            : i === data.length - 1
+                            ? answerBoxRight
+                            : undefined
+                        }
+                        className={
+                          i < values.shiftIndex + 1
+                            ? "relative text-red-300 before:absolute before:w-4 before:h-0.5 before:bg-red-300 before:-rotate-45 before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2"
+                            : !values.answer &&
+                              i === values.list.length - 1 &&
+                              i >= key.length
+                            ? "text-emerald-500"
+                            : "text-white"
+                        }
+                        key={i}
+                      >
+                        {value}
                       </span>
                     ))}
-                    {key.map((value, i) => (
-                      <span key={i}>{value}</span>
-                    ))}
                   </div>
-                )}
+
+                  {data.length > 0 && !values.answer && (
+                    <div className="relative text-gray-600">
+                      <span className="absolute -left-12 text-sm">Key:</span>
+                      {Array.from({ length: +level }).map((_, i) => (
+                        <span className="opacity-0" key={i}>
+                          0
+                        </span>
+                      ))}
+                      {key.map((value, i) => (
+                        <span key={i}>{value}</span>
+                      ))}
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           ))}
